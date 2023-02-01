da74dbb8ae7b337e3094e4ae52530529cc330d18
Juliana <71522623+juju-lee@users.noreply.github.com>
2023-02-01 18:50:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("#credit-card");
+const cardExpiration = document.querySelector("#valid-thru-text");
+const cardExpirationText = document.querySelector(".expiration-vl");
+
+cardExpiration.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardExpirationText.innerHTML = "02/40";
+    } else {
+        const valuesOfInput = e.target.value.replace("/", "");
+
+        if (e.target.value.length > 2) {
+            e.target.value = valuesOfInput.replace(/^(\d{2})(\d{0,2})/, "$1/$2");
+            cardExpirationText.innerHTML = valuesOfInput.replace(/^(\d{2})(\d{0,2})/, "$1/$2");
+        } else {
+            cardExpirationText.innerHTML = valuesOfInput;
+        }
+    }
+})
